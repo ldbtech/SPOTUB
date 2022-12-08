db45302e72c45f110d4b9db918ef3e99ceb12434
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2022-12-07 23:32:20 -0500

    Initial Commit

---
+++ SPOTUB/SPOTUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPOTUBApp.swift
+//  SPOTUB
+//
+//  Created by Ali Daho on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SPOTUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
